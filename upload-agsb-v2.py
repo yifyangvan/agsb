b336c42e08364dbb014b2e2c6178380027c5662b
yifyangvan <131954018+yifyangvan@users.noreply.github.com>
2025-06-06 13:51:56 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
-PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+USER_NAME = "uizz"         # 用户名
+UUID = "9a9c6dc3-2c22-4d1d-9852-c0432c90a6a6"                     # UUID，留空则自动生成
+PORT = 48001                   # Vmess端口，留空或0则自动生成
+DOMAIN = "streamlit.uicc.pp.ua"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiZDYwOGNiYjdhMjMwM2ZmOGNiZGEzN2Y0N2EwMDBhNWEiLCJ0IjoiYTZiMDNhMGMtYzA3Ny00YzE5LTk1YWEtMzAzYTJhNzZmNDVhIiwicyI6Ik9UZzNPV1l3TnpZdE9URTFPQzAwWkRRMkxUZ3pNek10WkRoaFlqWmxNakJtTjJJeiJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
